f385bf53427029feb4b28b1a2953c1c4c7b3d771
Luca Valente <luca.valente2@unibo.it>
2023-03-03 17:38:50 +0100

    cheshire.mk: Use root variable where needed

--- cheshire.mk
+++ cheshire.mk
@@ -16,7 +16,7 @@ VSIM        ?= vsim
 # Define used paths (prefixed to avoid name conflicts)
 CHS_ROOT      ?= $(shell $(BENDER) path cheshire)
 CHS_SW_DIR     = $(CHS_ROOT)/sw
-CHS_HW_DIR     = $(CHS_ROOT)/sw
+CHS_HW_DIR     = $(CHS_ROOT)/hw
 CHS_OTP_DIR    = $(shell $(BENDER) path opentitan_peripherals)
 CHS_CLINT_DIR  = $(shell $(BENDER) path clint)
 CHS_SLINK_DIR  = $(shell $(BENDER) path serial_link)
@@ -32,13 +32,13 @@ all: chs-sw-all chs-hw-all chs-sim-all chs-xilinx-all
 ################
 
 # Ensure both Bender dependencies and submodules are checked out
-.deps:
+$(CHS_ROOT)/.deps:
 	$(BENDER) checkout
 	git submodule update --init --recursive
 	@touch $@
 
 # Make sure this is done before any makefrags are included
-%.mk: .deps
+%.mk: $(CHS_ROOT)/.deps
 
 ######################
 # Nonfree components #
@@ -57,14 +57,14 @@ nonfree-init:
 # Build SW #
 ############
 
-include sw/sw.mk
+include $(CHS_SW_DIR)/sw.mk
 
 ###############
 # Generate HW #
 ###############
 
 # SoC registers
-hw/regs/cheshire_reg_pkg.sv hw/regs/cheshire_reg_top.sv: hw/regs/cheshire_regs.hjson
+$(CHS_ROOT)/hw/regs/cheshire_reg_pkg.sv $(CHS_ROOT)/hw/regs/cheshire_reg_top.sv: $(CHS_ROOT)/hw/regs/cheshire_regs.hjson
 	$(REGGEN) -r $< --outdir $(dir $@)
 
 # CLINT
@@ -75,7 +75,7 @@ $(CHS_CLINT_DIR)/.generated: Bender.yml
 
 # OpenTitan peripherals
 include $(CHS_OTP_DIR)/otp.mk
-$(CHS_OTP_DIR)/.generated: hw/rv_plic.cfg.hjson Bender.yml
+$(CHS_OTP_DIR)/.generated: $(CHS_ROOT)/hw/rv_plic.cfg.hjson Bender.yml
 	cp $< $(dir $@)/src/rv_plic/
 	$(MAKE) otp
 	@touch $@
@@ -87,12 +87,12 @@ $(CHS_VGA_DIR)/.generated: Bender.yml
 	@touch $@
 
 # Custom serial link
-$(CHS_SLINK_DIR)/.generated: hw/serial_link.hjson
+$(CHS_SLINK_DIR)/.generated: $(CHS_ROOT)/hw/serial_link.hjson
 	cp $< $(dir $@)/src/regs/serial_link_single_channel.hjson
 	$(MAKE) -C $(CHS_SLINK_DIR) update-regs
 	@touch $@
 
-chs-hw-all: hw/regs/cheshire_reg_pkg.sv hw/regs/cheshire_reg_top.sv
+chs-hw-all: $(CHS_ROOT)/hw/regs/cheshire_reg_pkg.sv $(CHS_ROOT)/hw/regs/cheshire_reg_top.sv
 chs-hw-all: $(CHS_CLINT_DIR)/.generated
 chs-hw-all: $(CHS_OTP_DIR)/.generated
 chs-hw-all: $(CHS_VGA_DIR)/.generated
@@ -105,53 +105,53 @@ chs-hw-all: $(CHS_SLINK_DIR)/.generated
 # This is *not* done as part of `all` as it is only reproducible with a specific compiler
 
 # Boot ROM (needs SW stack)
-CHS_BROM_SRCS = $(wildcard hw/bootrom/*.S hw/bootrom/*.c) $(CHS_SW_LIBS)
+CHS_BROM_SRCS = $(wildcard $(CHS_ROOT)/hw/bootrom/*.S $(CHS_ROOT)/hw/bootrom/*.c) $(CHS_SW_LIBS)
 CHS_BROM_FLAGS = $(RISCV_LDFLAGS) -Os -fno-zero-initialized-in-bss -flto -fwhole-program
 
-hw/bootrom/cheshire_bootrom.elf: hw/bootrom/cheshire_bootrom.ld $(CHS_BROM_SRCS)
+$(CHS_ROOT)/hw/bootrom/cheshire_bootrom.elf: $(CHS_ROOT)/hw/bootrom/cheshire_bootrom.ld $(CHS_BROM_SRCS)
 	$(RISCV_CC) $(CHS_SW_INCLUDES) -T$< $(CHS_BROM_FLAGS) -o $@ $(CHS_BROM_SRCS)
 
-hw/bootrom/cheshire_bootrom.sv: hw/bootrom/cheshire_bootrom.bin util/gen_bootrom.py
-	$(PYTHON3) util/gen_bootrom.py --sv-module cheshire_bootrom $< > $@
+$(CHS_ROOT)/hw/bootrom/cheshire_bootrom.sv: $(CHS_ROOT)/hw/bootrom/cheshire_bootrom.bin $(CHS_ROOT)/util/gen_bootrom.py
+	$(PYTHON3) $(CHS_ROOT)/util/gen_bootrom.py --sv-module cheshire_bootrom $< > $@
 
-chs-bootrom-all: hw/bootrom/cheshire_bootrom.sv hw/bootrom/cheshire_bootrom.dump
+chs-bootrom-all: $(CHS_ROOT)/hw/bootrom/cheshire_bootrom.sv $(CHS_ROOT)/hw/bootrom/cheshire_bootrom.dump
 
 ##############
 # Simulation #
 ##############
 
-target/sim/vsim/compile.cheshire_soc.tcl: Bender.yml
+$(CHS_ROOT)/target/sim/vsim/compile.cheshire_soc.tcl: Bender.yml
 	$(BENDER) script vsim -t sim -t cv64a6_imafdc_sv39 -t test -t cva6 --vlog-arg="$(VLOG_ARGS)" > $@
-	echo 'vlog "$(CURDIR)/target/sim/src/elfloader.cpp" -ccflags "-std=c++11"' >> $@
+	echo 'vlog "$(CURDIR)/$(CHS_ROOT)/target/sim/src/elfloader.cpp" -ccflags "-std=c++11"' >> $@
 
-target/sim/models:
+$(CHS_ROOT)/target/sim/models:
 	mkdir -p $@
 
 # Download (partially non-free) simulation models from publically available sources;
 # by running these targets or targets depending on them, you accept this (see README.md).
-target/sim/models/s25fs512s.sv: Bender.yml | target/sim/models
+$(CHS_ROOT)/target/sim/models/s25fs512s.sv: Bender.yml | $(CHS_ROOT)/target/sim/models
 	wget --no-check-certificate https://freemodelfoundry.com/fmf_vlog_models/flash/s25fs512s.sv -O $@
 	touch $@
 
-target/sim/models/24FC1025.v: Bender.yml | target/sim/models
+$(CHS_ROOT)/target/sim/models/24FC1025.v: Bender.yml | $(CHS_ROOT)/target/sim/models
 	wget https://ww1.microchip.com/downloads/en/DeviceDoc/24xx1025_Verilog_Model.zip -o $@
 	unzip -p 24xx1025_Verilog_Model.zip 24FC1025.v > $@
 	rm 24xx1025_Verilog_Model.zip
 
-target/sim/models/uart_tb_rx.sv: Bender.yml | target/sim/models
+$(CHS_ROOT)/target/sim/models/uart_tb_rx.sv: Bender.yml | $(CHS_ROOT)/target/sim/models
 	wget https://raw.githubusercontent.com/pulp-platform/pulp/v1.0/rtl/vip/uart_tb_rx.sv -O $@
 	touch $@
 
-chs-sim-all: target/sim/models/s25fs512s.sv
-chs-sim-all: target/sim/models/24FC1025.v
-chs-sim-all: target/sim/models/uart_tb_rx.sv
-chs-sim-all: target/sim/vsim/compile.cheshire_soc.tcl
+chs-sim-all: $(CHS_ROOT)/target/sim/models/s25fs512s.sv
+chs-sim-all: $(CHS_ROOT)/target/sim/models/24FC1025.v
+chs-sim-all: $(CHS_ROOT)/target/sim/models/uart_tb_rx.sv
+chs-sim-all: $(CHS_ROOT)/target/sim/vsim/compile.cheshire_soc.tcl
 
 #############
 # FPGA Flow #
 #############
 
-target/xilinx/scripts/add_sources.tcl: Bender.yml
+$(CHS_ROOT)/target/xilinx/scripts/add_sources.tcl: Bender.yml
 	$(BENDER) script vivado -t fpga -t cv64a6_imafdc_sv39 -t cva6 > $@
 
-chs-xilinx-all: target/xilinx/scripts/add_sources.tcl
+chs-xilinx-all: $(CHS_ROOT)/target/xilinx/scripts/add_sources.tcl
