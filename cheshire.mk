f6f53d8b9159beb5653efc77ac859f584e4a0d22
Jannis Schönleber <joennlae@gmail.com>
2023-04-21 10:08:07 +0200

    ci: update nonfree pointer

--- cheshire.mk
+++ cheshire.mk
@@ -47,7 +47,7 @@ include $(CHS_ROOT)/.deps
 ######################
 
 CHS_NONFREE_REMOTE ?= git@iis-git.ee.ethz.ch:pulp-restricted/cheshire-nonfree.git
-CHS_NONFREE_COMMIT ?= 379f427
+CHS_NONFREE_COMMIT ?= 8504d9b
 
 nonfree-init:
 	git clone $(CHS_NONFREE_REMOTE) nonfree
