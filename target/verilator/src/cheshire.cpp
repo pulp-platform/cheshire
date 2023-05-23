f000c395582e0a200a1b0c02e838abdb122204c0
Jannis Schönleber <joennlae@gmail.com>
2023-05-23 17:14:22 +0200

    style: clang-format

--- target/verilator/src/cheshire.cpp
+++ target/verilator/src/cheshire.cpp
@@ -41,57 +41,56 @@ static vluint64_t main_time = 0;
 int clk_ratio = 2;
 
 static void cycle_start(std::shared_ptr<Vcheshire_testharness> top) {
-  top->rtc_i = 1;
-  top->jtag_tck = 1;
-  for (int i = 0; i < clk_ratio; i++) {
-    top->clk_i = 1;
     top->rtc_i = 1;
-    printf("tick\n");
-    top->eval();
-    main_time += 2500;
-    top->clk_i = 0;
-    top->rtc_i = 0;
-    top->eval();
-    main_time += 2500;
-  }
+    top->jtag_tck = 1;
+    for (int i = 0; i < clk_ratio; i++) {
+        top->clk_i = 1;
+        top->rtc_i = 1;
+        printf("tick\n");
+        top->eval();
+        main_time += 2500;
+        top->clk_i = 0;
+        top->rtc_i = 0;
+        top->eval();
+        main_time += 2500;
+    }
 }
 
 static void cycle_end(std::shared_ptr<Vcheshire_testharness> top) {
-  top->rtc_i = 0;
-  for (int i = 0; i < clk_ratio; i++) {
-    top->clk_i = 1;
-    top->rtc_i = 1;
-    top->eval();
-    main_time += 2500;
-    top->clk_i = 0;
     top->rtc_i = 0;
-    top->eval();
-    main_time += 2500;
-  }
+    for (int i = 0; i < clk_ratio; i++) {
+        top->clk_i = 1;
+        top->rtc_i = 1;
+        top->eval();
+        main_time += 2500;
+        top->clk_i = 0;
+        top->rtc_i = 0;
+        top->eval();
+        main_time += 2500;
+    }
 }
 
-static void wait_cycles(std::shared_ptr<Vcheshire_testharness> top,
-                        int cycles) {
-  for (int i = 0; i < cycles; i++) {
-    cycle_start(top);
-    cycle_end(top);
-  }
+static void wait_cycles(std::shared_ptr<Vcheshire_testharness> top, int cycles) {
+    for (int i = 0; i < cycles; i++) {
+        cycle_start(top);
+        cycle_end(top);
+    }
 }
 
 int main(int argc, char **argv) {
 
-  Verilated::commandArgs(argc, argv);
-  std::shared_ptr<Vcheshire_testharness> top(new Vcheshire_testharness);
+    Verilated::commandArgs(argc, argv);
+    std::shared_ptr<Vcheshire_testharness> top(new Vcheshire_testharness);
 
-  // reset
-  for (int i = 0; i < 10; i++) {
-    top->rst_ni = 0;
-    top->rtc_i = 0;
-    wait_cycles(top, 5);
-  }
-  top->rst_ni = 1;
+    // reset
+    for (int i = 0; i < 10; i++) {
+        top->rst_ni = 0;
+        top->rtc_i = 0;
+        wait_cycles(top, 5);
+    }
+    top->rst_ni = 1;
 
-  top->final();
+    top->final();
 
-  return 0;
+    return 0;
 }
