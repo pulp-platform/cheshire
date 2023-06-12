1300341618ee9b6cfae503b1023a0346597ac399
CyrilKoe <47349427+CyrilKoe@users.noreply.github.com>
2023-06-12 13:22:02 +0200

    target/xilinx: Boot on Genesys 2 in internal CI (#43)
    * fpga: Program genesys on bordcomputer
    
    * fixup: Point to merged nonfree branch
    
    ---------
    
    Co-authored-by: Cyril Koenig <cykoenig@iis.ee.ethz.ch>
    Co-authored-by: Paul Scheffler <paulsc@iis.ee.ethz.ch>

--- target/xilinx/scripts/program.tcl
+++ target/xilinx/scripts/program.tcl
@@ -5,22 +5,22 @@
 # Author: Florian Zaruba <zarubaf@iis.ee.ethz.ch>
 # Description: Program Genesys II
 
-open_hw
+open_hw_manager
 
-connect_hw_server -url localhost:3121
+connect_hw_server -url $::env(HOST):$::env(PORT)
 
 if {$::env(BOARD) eq "genesys2"} {
-  open_hw_target {localhost:3121/xilinx_tcf/Digilent/200300A8CD43B}
+  open_hw_target $::env(HOST):$::env(PORT)/$::env(FPGA_PATH)
 
   current_hw_device [get_hw_devices xc7k325t_0]
-  set_property PROGRAM.FILE {$::env(BIT)} [get_hw_devices xc7k325t_0]
+  set_property PROGRAM.FILE $::env(BIT) [get_hw_devices xc7k325t_0]
   program_hw_devices [get_hw_devices xc7k325t_0]
   refresh_hw_device [lindex [get_hw_devices xc7k325t_0] 0]
 } elseif {$::env(BOARD) eq "vc707"} {
-  open_hw_target {localhost:3121/xilinx_tcf/Digilent/210203A5FC70A}
+  open_hw_target {$::env(HOST):$::env(PORT)/$::env(FPGA_PATH)}
 
   current_hw_device [get_hw_devices xc7vx485t_0]
-  set_property PROGRAM.FILE {$::env(BIT)} [get_hw_devices xc7vx485t_0]
+  set_property PROGRAM.FILE $::env(BIT) [get_hw_devices xc7vx485t_0]
   program_hw_devices [get_hw_devices xc7vx485t_0]
   refresh_hw_device [lindex [get_hw_devices xc7vx485t_0] 0]
 } else {
