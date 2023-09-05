c17cc41e53199067a714b6659619352ca9e780da
aottaviano <aottaviano@iis.ee.ethz.ch>
2023-09-05 16:31:31 +0200

    sw: Add simple test to DSA dma

---
+++ sw/tests/dsa_dma_test.c
@@ -0,0 +1,59 @@
+// Copyright 2023 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Alessandro Ottaviano <aottaviano@iis.ee.ethz.ch>
+//
+
+#include "io.h"
+#include "dif/clint.h"
+#include "dif/uart.h"
+#include "params.h"
+#include "regs/cheshire.h"
+#include "dma.h"
+#include "util.h"
+
+#define NUM_INITIALIZED_VALUES 400
+
+int main(void) {
+
+//    // Disable D-Cache
+//    asm volatile(
+//            "addi t0, x0, 1\n   \
+//             csrrc x0, 0x701, t0\n"
+//             ::: "t0"
+//    );
+
+    // Size of transfer
+    uint32_t size_bytes = 512;
+
+    volatile uint64_t *spm_free = 0x10000000 + 0x4000000;
+    volatile uint64_t *dram_free = 0x80000000 + 0x4000000;
+    volatile uint64_t *spm_expected = spm_free;
+
+    // Initialize spm and dram free regions
+    for(int i = 0; i < NUM_INITIALIZED_VALUES; i++)
+    {
+        spm_free[i] = 0xcafedeadbaadf00d + i;
+	spm_expected[i] = spm_free[i]; // golden values
+    }
+
+    // write from spm to dram (blocking)
+    dsa0_dma_blk_memcpy(dram_free, spm_free, size_bytes);
+    fence();
+
+    // write from dram to spm (blocking)
+    dsa0_dma_blk_memcpy(spm_free, dram_free, size_bytes);
+    fence();
+
+    // check spm destination against expected
+
+    // if something went wrong in the W/R journey towards dram, data
+    // will be different
+
+    for(int i = 0; i < NUM_INITIALIZED_VALUES; i++)
+    {
+	CHECK_ASSERT(1, spm_free[i] == spm_expected[i]);
+    }
+
+}
