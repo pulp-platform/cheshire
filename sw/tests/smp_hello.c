53d71dec1a197ff612920a0d9a0722895221f8a5
Emanuele Parisi <emanuele.parisi@protonmail.com>
2023-11-21 23:03:36 +0100

    Fix undeclared __BAUDRATE after rebasing onto master

--- sw/tests/smp_hello.c
+++ sw/tests/smp_hello.c
@@ -38,7 +38,7 @@ void semaphore_post() {
 int main(void) {
     uint32_t rtc_freq = *reg32(&__base_regs, CHESHIRE_RTC_FREQ_REG_OFFSET);
     uint64_t reset_freq = clint_get_core_freq(rtc_freq, 2500);
-    uart_init(&__base_uart, reset_freq, __BAUDRATE);
+    uart_init(&__base_uart, reset_freq, 115200);
 
     smp_resume();
 
