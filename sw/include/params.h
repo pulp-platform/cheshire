1efeabaa5cd802384ec1f1265a9dbf397a0dc300
Paul Scheffler <paulsc@iis.ee.ethz.ch>
2024-02-07 21:24:36 +0100

    sw: Add parameter for default boot baudrate

--- sw/include/params.h
+++ sw/include/params.h
@@ -30,6 +30,9 @@ extern void *__base_axirtgrd;
 extern void *__base_spm;
 extern void *__base_dram;
 
+// Default boot baudrate
+static const uint32_t __BOOT_BAUDRATE = 115200;
+
 // Maximum number of LBAs to copy to SPM for boot (48 KiB)
 static const uint64_t __BOOT_SPM_MAX_LBAS = 2 * 48;
 
--- sw/tests/helloworld.c
+++ sw/tests/helloworld.c
@@ -17,7 +17,7 @@ int main(void) {
     char str[] = "Hello World!\r\n";
     uint32_t rtc_freq = *reg32(&__base_regs, CHESHIRE_RTC_FREQ_REG_OFFSET);
     uint64_t reset_freq = clint_get_core_freq(rtc_freq, 2500);
-    uart_init(&__base_uart, reset_freq, 115200);
+    uart_init(&__base_uart, reset_freq, __BOOT_BAUDRATE);
     uart_write_str(&__base_uart, str, sizeof(str));
     uart_write_flush(&__base_uart);
     return 0;
