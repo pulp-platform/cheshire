f0cede011bb10430a0c21942556663eb90dc78c3
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2024-03-13 16:22:39 +0100

    target/sim: add Ara test configuration

--- target/sim/src/tb_cheshire_pkg.sv
+++ target/sim/src/tb_cheshire_pkg.sv
@@ -23,11 +23,21 @@ package tb_cheshire_pkg;
       return ret;
     endfunction
 
+    // A dedicated Ara config
+    function automatic cheshire_cfg_t gen_cheshire_ara_cfg();
+      cheshire_cfg_t ret = DefaultCfg;
+      ret.Ara        = 1;
+      ret.AraNrLanes = 2;
+      ret.AraVLEN    = 2048;
+      return ret;
+    endfunction
+
     // Number of Cheshire configurations
-    localparam int unsigned NumCheshireConfigs = 32'd3;
+    localparam int unsigned NumCheshireConfigs = 32'd4;
 
     // Assemble a configuration array indexed by a numeric parameter
     localparam cheshire_cfg_t [NumCheshireConfigs-1:0] TbCheshireConfigs = {
+        gen_cheshire_ara_cfg(),  // 3: Ara-enabled configuration
         gen_cheshire_clic_cfg(), // 2: CLIC-enabled configuration
         gen_cheshire_rt_cfg(),   // 1: RT-enabled configuration
         DefaultCfg               // 0: Default configuration
