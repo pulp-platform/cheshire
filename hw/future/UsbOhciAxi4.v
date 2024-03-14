a1f470a836d2054a7797bfafe029ddba5e5f4205
Paul Scheffler <paulsc@iis.ee.ethz.ch>
2024-03-14 22:56:42 +0100

    treewide: Update license information

--- hw/future/UsbOhciAxi4.v
+++ hw/future/UsbOhciAxi4.v
@@ -1,3 +1,37 @@
+// MIT License
+
+// Copyright (c) 2016 Spinal HDL contributors
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// ------
+// The Verilog RTL below this comment block was generated using the SpinalHDL
+// library (licensed under the permissive MIT license above) available here:
+//
+// https://github.com/SpinalHDL/SpinalHDL
+//
+// The following file from the SpinalHDL library was used for generation (the
+// generator version and git commit are specified in the generated output):
+//
+// lib/src/main/scala/spinal/lib/com/usb/ohci/UsbOhciAxi4.scala
+// ------
+
 // Generator : SpinalHDL v1.9.4    git head : 270018552577f3bb8e5339ee2583c9c22d324215
 // Component : UsbOhciAxi4
 
