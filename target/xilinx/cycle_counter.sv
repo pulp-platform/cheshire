94f03e150444003fd85c89e92e95e71e63e1e7e3
bsc24f15 Illian Grünberg (igruenberg) <bsc24f15@badile38.ee.ethz.ch>
2024-04-12 10:32:43 +0200

    32bit-counters

---
+++ target/xilinx/cycle_counter.sv
@@ -0,0 +1,47 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Illian Gruenberg
+// 
+// Create Date: 03/14/2024 05:34:34 PM
+// Design Name: 
+// Module Name: 
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module cycle_counter #(
+    
+)
+(
+    input rst_ni,
+    input clk_i
+);
+ (* dont_touch = "yes" *) (* mark_debug = "true" *) logic [63:0] counter_clk_cycles_q;
+ logic [63:0] counter_clk_cycles_d;
+ 
+ initial begin
+    counter_clk_cycles_d = 64'b0;
+    counter_clk_cycles_q = 64'b0;
+ end
+ always @(negedge rst_ni )begin
+        counter_clk_cycles_d <= 0;
+        counter_clk_cycles_q <= 0;
+ end
+always_ff @(posedge clk_i) begin
+    if(rst_ni) begin
+    counter_clk_cycles_q <= counter_clk_cycles_d + 1;
+    counter_clk_cycles_d <= counter_clk_cycles_q;
+    end 
+end
+
+endmodule
--- target/xilinx/src/axi_snoop.sv
+++ target/xilinx/src/axi_snoop.sv
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
+// Company: ETH Zuerich
 // Engineer: Illian Gruenberg
 // 
 // Create Date: 03/14/2024 05:34:34 PM
@@ -20,11 +20,13 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
+
 module axi_snoop #(
 parameter type axi_mst_req_t = logic,
 parameter type axi_mst_rsp_t = logic
 
-)(
+)(  
+    input rst_ni,
     input clk_i,
     input axi_mst_req_t  axi_mst_req_i,
     input axi_mst_rsp_t  axi_mst_rsp_i,
@@ -33,44 +35,53 @@ parameter type axi_mst_rsp_t = logic
 
     );
       
-    (* dont_touch = "yes" *) (* mark_debug = "true" *) logic [63:0] counter_reg_q ;
-    logic [63:0] counter_reg_d ;
-    /*(* dont_touch = "yes" *) (* mark_debug = "true" *) logic [63:0] counter_stall_q ;
-    logic [63:0] counter_stall_d ;
-    */
+    (* dont_touch = "yes" *) (* mark_debug = "true" *) logic [31:0] counter_reg_q ;
+    logic [31:0] counter_reg_d ;
+    (* dont_touch = "yes" *) (* mark_debug = "true" *) logic [31:0] counter_stall_q ;
+    logic [31:0] counter_stall_d ;
+    
     initial begin
-      counter_reg_q = 64'b0;
-      counter_reg_d = 64'b0;
-      /*counter_stall_q = 64'b0;
-      counter_stall_d = 64'b0;*/
+      counter_reg_q = 32'b0;
+      counter_reg_d = 32'b0;
+      counter_stall_q = 32'b0;
+      counter_stall_d = 32'b0;
     end
- 
+    
+    
     always_ff @(posedge clk_i) begin
-
-        if (axi_mst_req_i.aw_valid & axi_mst_rsp_i.aw_ready) begin
-            counter_reg_q <= counter_reg_d + 1;
-            counter_reg_d <= counter_reg_q;
-        end
+            if (axi_mst_rsp_i.r_valid & axi_mst_req_i.r_ready) begin
+                counter_reg_q <= counter_reg_d + 1;
+                counter_reg_d <= counter_reg_q;
+            end
        
-        if (axi_mst_rsp_i.b_valid & axi_slv_rsp_i.b_ready) begin
-            counter_reg_q <= counter_reg_d + 1;
-            counter_reg_d <= counter_reg_q;
-        end 
+            if (axi_mst_rsp_i.b_valid & axi_mst_req_i.b_ready) begin
+                counter_reg_q <= counter_reg_d + 1;
+                counter_reg_d <= counter_reg_q;
+            end
+
+            if (!rst_ni)begin
+            counter_reg_d <= 0;
+            counter_reg_q <= 0;
+            end
     end
 
-    /*always_ff @(posedge clk_i) begin
-
-        if (axi_mst_req_i.aw_valid & !(axi_mst_rsp_i.aw_ready)) begin
-            counter_stall_q <= counter_stall_d + 1;
-            counter_stall_d <= counter_stall_q;
-        end
+    always_ff @(posedge clk_i) begin
+            if (axi_mst_rsp_i.r_valid & !(axi_mst_req_i.r_ready)) begin
+                counter_stall_q <= counter_stall_d + 1;
+                counter_stall_d <= counter_stall_q;
+            end
        
-        if (axi_mst_rsp_i.b_valid & !(axi_slv_rsp_i.b_ready)) begin
-            counter_stall_q <= counter_stall_d + 1;
-            counter_stall_d <= counter_stall_q;
-        end 
+            if (axi_mst_rsp_i.b_valid & !(axi_mst_req_i.b_ready)) begin
+                counter_stall_q <= counter_stall_d + 1;
+                counter_stall_d <= counter_stall_q;
+            end
+
+            if (!rst_ni)begin
+            counter_stall_d <= 0;
+            counter_stall_q <= 0;
+            end
     end
-    */
+    
     assign axi_mst_req_o = axi_mst_req_i;
     assign axi_mst_rsp_o = axi_mst_rsp_i;
 
