595c20ec7724f080eb1ea79b14a9bf4eb2d835da
Chaoqun Liang <chaoqun.liang@unibo.it>
2024-04-22 21:19:19 +0200

    test fix

--- sw/tests/idma.c
+++ sw/tests/idma.c
@@ -10,7 +10,7 @@
 
 #define IDMA_BASE          0x01000000
 #define SRC_ADDR           0x14000000
-#define DST_ADDR           0x14100000
+#define DST_ADDR           0x14000100
 
 #define IDMA_SRC_ADDR_OFFSET         0x000000d8
 #define IDMA_DST_ADDR_OFFSET         0x000000d0
@@ -20,23 +20,22 @@
 #define IDMA_CONF                    0x00000000
 
 int main() {
-    
+
     if (hart_id() != 0) wfi();
-    
-    printf("Start iDMA test...\n\r");
+    //printf("Start iDMA test...\n\r");
 
     volatile uint64_t src_data[8] = {
-        0x1032207098001032, 
+        0x1032207098001032,
         0x3210E20020709800,
-        0x1716151413121110, 
+        0x1716151413121110,
         0x2726252423222120,
-        0x3736353433323130, 
+        0x3736353433323130,
         0x4746454443424140,
-        0x5756555453525150, 
+        0x5756555453525150,
         0x6766656463626160
     };
 
-    // load data into src address 
+    // load data into src address
     for (int i = 0; i < 8; ++i) {
         volatile uint64_t *src_addr = (volatile uint64_t*)(SRC_ADDR + i * sizeof(uint64_t));
         *src_addr = src_data[i];
@@ -50,19 +49,22 @@ int main() {
     ptr = (int *)(IDMA_BASE + IDMA_LENGTH_OFFSET);
     *ptr = 0x00000040;
     ptr = (int *)(IDMA_BASE + IDMA_CONF);
-    *ptr = 0x3 << 10;
+    *ptr = 0x1 << 10;
     ptr = (int *)(IDMA_BASE + IDMA_REPS_2);
     *ptr = 0x00000001;
     ptr = (int *)(IDMA_BASE + IDMA_NEXT_ID_OFFSET);
     int id = *ptr;  // Read IDMA next ID
     
+    int err = 0;
 	for (int i = 0; i < 8; ++i) {
 	    volatile uint64_t *dst_addr = (volatile uint64_t*)(DST_ADDR + i * sizeof(uint64_t));
 	    uint64_t dst_data = *dst_addr;
 	    if (dst_data != src_data[i]) {
-	        printf("Data mismatch at index %d: expected 0x%llx, got 0x%llx\n\r", i, src_data[i], dst_data);
-
+	        err++;
 	    }
 	}
-    return 0;
+
+    if(err!= 0) {
+        printf("idma failed\n");
+    } else return 0;
 }
