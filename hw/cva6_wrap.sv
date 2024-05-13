2d8a9cf5822625fdf6c50fd35c29baabc1530089
Zexin Fu <zexifu@iis.ee.ethz.ch>
2024-05-13 03:57:48 +0200

    Initial ecc llc cheshire integration

--- hw/cva6_wrap.sv
+++ hw/cva6_wrap.sv
@@ -157,8 +157,8 @@ if (NumHarts > 1) begin: gen_multicore_hmr
     // .core_backup_t     ( '0 ), // TODO
     // .bus_outputs_t     ( '0 ), // TODO
     .reg_req_t         ( reg_req_t ), // TODO
-    .reg_rsp_t         ( reg_rsp_t ), // TODO
-    .rapid_recovery_t  ( rapid_recovery_pkg::rapid_recovery_t ) // TODO
+    .reg_rsp_t         ( reg_rsp_t ) // TODO
+    // .rapid_recovery_t  ( rapid_recovery_pkg::rapid_recovery_t ) // TODO
   ) i_cva6_hmr (
     .clk_i              ( clk_i         ),
     .rst_ni             ( rstn_i        ),
