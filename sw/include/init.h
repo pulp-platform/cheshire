da2e65e685ac4ded2e17a1c4287a5bf54ad45d05
Chaoqun Liang <chaoqun.liang@unibo.it>
2024-05-18 22:39:03 +0200

    sim new idma

--- sw/include/init.h
+++
@@ -1,7 +0,0 @@
-// Copyright 2022 ETH Zurich and University of Bologna.
-// Licensed under the Apache License, Version 2.0, see LICENSE for details.
-// SPDX-License-Identifier: Apache-2.0
-
-#include "cheshire_io.h"
-
-void soc_init();
