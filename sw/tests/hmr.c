012f2d424c731bf0ea294373d7fa44b9b06c9143
Yvan Tortorella <ytortorella@lagrev3.ee.ethz.ch>
2024-06-08 16:49:41 +0200

    Cleanup the HMR test.

--- sw/tests/hmr.c
+++ sw/tests/hmr.c
@@ -13,23 +13,16 @@
 
 int main(void) {
 
-  // We read the number of available harts.
-  uint32_t NumHarts = *reg32(&__base_regs, CHESHIRE_NUM_INT_HARTS_REG_OFFSET);
-  uint32_t OtherHart = NumHarts - 1 - hart_id(); // If hart_id() == 0 -> return 1;
-                                                 // If hart_id() == 1 -> return 0;
-
   // Hart 0 enters first
   printf("%d!\n", hart_id());
 
   // Only hart 0 gets here
   if (hart_id() == 0){
     // Check if DMR is enabled. If not, enable it
-    if (!(*reg32(&__base_hmr, HMR_DMR_ENABLE))) {
+    if (!(*reg32(&__base_hmr, HMR_DMR_ENABLE)))
       *reg32(&__base_hmr, HMR_DMR_ENABLE) = 0x1;
-      // Wake up sleeping hart
-      // wakeup_hart(OtherHart);
-      smp_resume();
-    }
+    // Wake up the SMP core
+    smp_resume();
   }
 
   chs_hmr_store_state(); // -> Save state on top of the stack
