45417fc2a46ad3f19e126e9b6b3f8068d074e4e2
Yvan Tortorella <ytortorella@lagrev3.ee.ethz.ch>
2024-07-05 12:22:17 +0200

    Add UART init in HMR main.

--- sw/tests/hmr.c
+++ sw/tests/hmr.c
@@ -7,11 +7,17 @@
 // Test for HMR functionalities.
 
 #include "regs/cheshire.h"
+#include "dif/clint.h"
+#include "dif/uart.h"
 #include "util.h"
 #include "hmr.h"
 #include "printf.h"
 
 int main(void) {
+
+    uint32_t rtc_freq = *reg32(&__base_regs, CHESHIRE_RTC_FREQ_REG_OFFSET);
+    uint64_t reset_freq = clint_get_core_freq(rtc_freq, 2500);
+    uart_init(&__base_uart, reset_freq, 115200);
 
     // Hart 0 enters first
     printf("%d!\n", hart_id());
