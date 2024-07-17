e0fc9b1015a8440cb6a236871528fb323ab7a59a
Nils Wistoff <nwistoff@iis.ee.ethz.ch>
2024-07-17 19:05:21 +0200

    ci: Bump cheshire-nonfree
    Signed-off-by: Nils Wistoff <nwistoff@iis.ee.ethz.ch>

--- cheshire.mk
+++ cheshire.mk
@@ -57,7 +57,7 @@ chs-clean-deps:
 ######################
 
 CHS_NONFREE_REMOTE ?= git@iis-git.ee.ethz.ch:pulp-restricted/cheshire-nonfree.git
-CHS_NONFREE_COMMIT ?= cba34e2
+CHS_NONFREE_COMMIT ?= f731b17
 
 chs-nonfree-init:
 	git clone $(CHS_NONFREE_REMOTE) $(CHS_ROOT)/nonfree
