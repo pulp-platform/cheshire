0b1a3f0b2fbb2b318f4cd218cbf276effa6fc0d8
Yvan Tortorella <ytortorella@lagrev3.ee.ethz.ch>
2024-07-28 18:56:11 +0200

    Update nonfree.

--- cheshire.mk
+++ cheshire.mk
@@ -58,7 +58,7 @@ chs-clean-deps:
 ######################
 
 CHS_NONFREE_REMOTE ?= git@iis-git.ee.ethz.ch:pulp-restricted/cheshire-nonfree.git
-CHS_NONFREE_COMMIT ?= d0a0c9a # branch: astral
+CHS_NONFREE_COMMIT ?= fc52b473 # branch: astral
 
 chs-nonfree-init:
 	git clone $(CHS_NONFREE_REMOTE) $(CHS_ROOT)/nonfree
