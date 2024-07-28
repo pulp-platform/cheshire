36b66763d68bca8c48f89b885964ea67b6b9c728
Yvan Tortorella <ytortorella@lagrev3.ee.ethz.ch>
2024-07-28 17:02:20 +0200

    Add SoC initialization into crt0.

--- sw/tests/hmr.c
+++ sw/tests/hmr.c
@@ -15,10 +15,6 @@
 
 int main(void) {
 
-    uint32_t rtc_freq = *reg32(&__base_regs, CHESHIRE_RTC_FREQ_REG_OFFSET);
-    uint64_t reset_freq = clint_get_core_freq(rtc_freq, 2500);
-    uart_init(&__base_uart, reset_freq, 115200);
-
     // Hart 0 enters first
     printf("%d!\n", hart_id());
 
