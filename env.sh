c95de299302bbafaa53156351df0443ce36dc4bc
Yvan Tortorella <ytortorella@lagrev5.ee.ethz.ch>
2024-10-09 17:02:06 +0200

    Add few utilities for simulation purposes.

---
+++ env.sh
@@ -0,0 +1,2 @@
+# Export RV64 GCC toolchain for ETH workspace.
+export PATH=/usr/pack/riscv-1.0-kgf/riscv64-gcc-11.2.0/bin:$PATH # RV64 GCC toolchain
---
+++ target/sim/vsim/launch.tcl
@@ -0,0 +1,5 @@
+set PRELMODE 0
+set BOOTMODE 0
+set BINARY ../../../sw/tests/helloworld.dram.elf
+source compile.cheshire_soc.tcl
+source start.cheshire_soc.tcl
