52a1521694ee9516cfe9ade8f3f572ff42c39d7c
Nils Wistoff <nwistoff@iis.ee.ethz.ch>
2024-11-15 00:12:34 +1100

    sw/tests: Add basic CLIC tests
    Signed-off-by: Nils Wistoff <nwistoff@iis.ee.ethz.ch>

--- target/sim/src/tb_cheshire_pkg.sv
+++ target/sim/src/tb_cheshire_pkg.sv
@@ -16,11 +16,19 @@ package tb_cheshire_pkg;
       return ret;
     endfunction
 
+    // A dedicated CLIC config
+    function automatic cheshire_cfg_t gen_cheshire_clic_cfg();
+      cheshire_cfg_t ret = DefaultCfg;
+      ret.Clic = 1;
+      return ret;
+    endfunction
+
     // Number of Cheshire configurations
-    localparam int unsigned NumCheshireConfigs = 32'd2;
+    localparam int unsigned NumCheshireConfigs = 32'd3;
 
     // Assemble a configuration array indexed by a numeric parameter
     localparam cheshire_cfg_t [NumCheshireConfigs-1:0] TbCheshireConfigs = {
+        gen_cheshire_clic_cfg(), // 2: CLIC-enabled configuration
         gen_cheshire_rt_cfg(),   // 1: RT-enabled configuration
         DefaultCfg               // 0: Default configuration
     };
