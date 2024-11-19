a7e3e65eeaabf9c5d599bd9df927cf0c5ff520bb
aottaviano <aottaviano@iis.ee.ethz.ch>
2024-11-19 09:49:24 +0100

    sw/include: Add manager ID discovery from hardware features
    Add utility function to get the manager id of cheshire according to the
    implemented HW features, thus removing any hardcoded
    parametrization (the number of application-class cores, as well as the
    specific hardware features, are configurable, hence not known a priori)

--- sw/include/util.h
+++ sw/include/util.h
@@ -5,10 +5,13 @@
 // Nicole Narr <narrn@student.ethz.ch>
 // Christopher Reinwardt <creinwar@student.ethz.ch>
 // Paul Scheffler <paulsc@iis.ee.ethz.ch>
+// Alessandro Ottaviano <aottaviano@iis.ee.ethz.ch>
 
 #pragma once
 
 #include <stdint.h>
+#include "regs/cheshire.h"
+#include "params.h"
 
 static inline volatile uint8_t *reg8(void *base, int offs) {
     return (volatile uint8_t *)(base + offs);
@@ -75,8 +78,63 @@ static inline void *gprw(void *gp) {
         if (__ccret) return __ccret; \
     }
 
-// If a condition; if it is untrue, ummediately return an error code.
+// If a condition; if it is untrue, immediately return an error code.
 #define CHECK_ASSERT(ret, cond) \
     if (!(cond)) return (ret);
 
 #define MIN(a, b) (((a) <= (b)) ? (a) : (b))
+
+// Bit manipulation
+#define BIT(n) (1UL << (n))
+#define BIT_MASK(n) (BIT(n) - 1)
+
+// Check if an hardware feature is present from software
+static inline uint32_t hw_feature_present(uint32_t bit) {
+    uint32_t features_bitmap = *reg32(&__base_regs, CHESHIRE_HW_FEATURES_REG_OFFSET);
+    return (features_bitmap & (1 << bit)) != 0;
+}
+
+// Helpers to handle Cheshire AXI managers attached to the system crossbar.
+
+// Cheshire managers. We label only core0 as the other cores - if any - are
+// contiguous after it by default.
+enum chs_mngrs {
+    CHS_MNGR_CORE0 = 0,
+    CHS_MNGR_DEBUG,
+    CHS_MNGR_DMA,
+    CHS_MNGR_SERIAL_LINK,
+    CHS_MNGR_VGA,
+    CHS_MNGR_USB
+};
+
+// Get Cheshire manager ID based on the hardware feature. If an hardware feature
+// is not implemented, returns `IMPL_ERR`.
+static inline uint32_t get_chs_mngr_id_from_hw_feature(enum chs_mngrs feature) {
+    uint32_t num_harts = *reg32(&__base_regs, CHESHIRE_NUM_INT_HARTS_REG_OFFSET);
+    uint32_t current_id = num_harts;
+
+    // Determine ID based on the input feature
+    switch (feature) {
+    case CHS_MNGR_CORE0:
+        return 0; // Core 0 ID is always 0
+        break;
+    case CHS_MNGR_DEBUG:
+        return num_harts; // Debug ID is always after cores
+        break;
+    case CHS_MNGR_DMA:
+        return hw_feature_present(CHESHIRE_HW_FEATURES_DMA_BIT) ? ++current_id : HW_IMPL_ERR;
+        break;
+    case CHS_MNGR_SERIAL_LINK:
+        return hw_feature_present(CHESHIRE_HW_FEATURES_SERIAL_LINK_BIT) ? ++current_id
+                                                                        : HW_IMPL_ERR;
+        break;
+    case CHS_MNGR_VGA:
+        return hw_feature_present(CHESHIRE_HW_FEATURES_VGA_BIT) ? ++current_id : HW_IMPL_ERR;
+        break;
+    case CHS_MNGR_USB:
+        return hw_feature_present(CHESHIRE_HW_FEATURES_USB_BIT) ? ++current_id : HW_IMPL_ERR;
+        break;
+    default:
+        return HW_IMPL_ERR; // Unknown feature
+    }
+}
