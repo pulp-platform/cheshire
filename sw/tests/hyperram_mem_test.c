ec31b908042882e8f3ca3c13cb2206af8fe4d48b
fabianaufdermaur <fabian.aufdermaur@bluewin.ch>
2024-11-25 15:57:56 +0100

    hyperram test angepasst

--- sw/tests/hyperram_mem_test.c
+++ sw/tests/hyperram_mem_test.c
@@ -14,19 +14,20 @@
 #include <stdint.h>
 
 #define HYPERRAM_START_ADDRESS 0x80000000
-#define HYPERRAM_END_ADDRESS 0x81000000
-#define HYPERRAM_NUM_ADDRESSES 256
+#define HYPERRAM_END_ADDRESS 0x82000000
+//#define HYPERRAM_NUM_ADDRESSES 256
 #define HYPERRAM_STEP_SIZE 4
+#define MIN_PRINTS 10
 
 int main(void) {
     __asm volatile("csrrwi x0, 0x7C1, 0\n");
 
     char start_msg[] = "Starting RAM test\r\n";
-    char reset_msg[] = "Gesamter RAM auf '-1' gesetzt\r\n";
+    char reset_msg[] = "Gesamter RAM auf '0' gesetzt\r\n";
     char write_msg[] = "Gesamter RAM beschrieben\r\n";
     char finish_msg[] = "RAM test finished\r\n";
-    char value_msg[64];
-    char msg[64];
+    char value_msg[128];
+    char msg[128];
 
     uint32_t rtc_freq = *reg32(&__base_regs, CHESHIRE_RTC_FREQ_REG_OFFSET);
     uint64_t reset_freq = clint_get_core_freq(rtc_freq, 2500);
@@ -35,43 +36,67 @@ int main(void) {
     uart_write_str(&__base_uart, start_msg, sizeof(start_msg));
     uart_write_flush(&__base_uart);
 
-    // Address Map auf -1 setzen
-    for (uint32_t addr = HYPERRAM_START_ADDRESS + HYPERRAM_STEP_SIZE; addr < HYPERRAM_END_ADDRESS; addr += HYPERRAM_STEP_SIZE) {
-        *(volatile uint32_t*)addr = (uint32_t)-1;
+    // Address Map auf 0 setzen
+    uint32_t iterations = 1;
+    for (uint32_t addr = HYPERRAM_START_ADDRESS; addr < HYPERRAM_END_ADDRESS; addr += HYPERRAM_STEP_SIZE) {
+        *(volatile uint32_t*)addr = (uint32_t)0;
+
+        if (iterations > MIN_PRINTS) {
+            snprintf(value_msg, sizeof(value_msg), 
+                    "Adresse: 0x%08X, Written Value: %u\r\n", 
+                    addr, *(volatile uint32_t*)addr);
+
+            uart_write_str(&__base_uart, value_msg, sizeof(value_msg));
+            uart_write_flush(&__base_uart);
+        }
+        iterations++;
     }
 
     uart_write_str(&__base_uart, reset_msg, sizeof(reset_msg));
     uart_write_flush(&__base_uart);
 
+    iterations = 1;
     for (uint32_t addr = HYPERRAM_START_ADDRESS; addr < HYPERRAM_END_ADDRESS; addr += HYPERRAM_STEP_SIZE) {
         volatile uint32_t value = *(volatile uint32_t*)addr;
-        if (value != (uint32_t)-1) {
+        if (value != (uint32_t)0) {
             snprintf(msg, sizeof(msg), 
-                     "ERROR - Adresse: 0x%08X, Value: %u, Expected: -1\r\n", 
+                     "ERROR - Adresse: 0x%08X, Value: %u, Expected: 0\r\n", 
                      addr, value);
             uart_write_str(&__base_uart, msg, sizeof(msg));
             uart_write_flush(&__base_uart);
         }
 
-        *(volatile uint32_t*)addr = (addr - HYPERRAM_START_ADDRESS) / HYPERRAM_STEP_SIZE;
+        *(volatile uint32_t*)addr = iterations;
+
+        if (iterations < MIN_PRINTS) {
+            snprintf(value_msg, sizeof(value_msg), 
+                    "Adresse: 0x%08X, Written Value: %u\r\n", 
+                    addr, *(volatile uint32_t*)addr);
+
+            uart_write_str(&__base_uart, value_msg, sizeof(value_msg));
+            uart_write_flush(&__base_uart);
+        }
+        iterations++;
     }
 
     uart_write_str(&__base_uart, write_msg, sizeof(write_msg));
     uart_write_flush(&__base_uart);
 
+    iterations = 1;
     for (uint32_t addr = HYPERRAM_START_ADDRESS; addr < HYPERRAM_END_ADDRESS; addr += HYPERRAM_STEP_SIZE) {
         volatile uint32_t read_value = *(volatile uint32_t*)addr;
-        uint32_t expected_value = (addr - HYPERRAM_START_ADDRESS) / HYPERRAM_STEP_SIZE;
+        uint32_t expected_value = iterations;
 
-        if (expected_value != read_value)
+        if (expected_value != read_value || iterations < MIN_PRINTS)
         {
             snprintf(value_msg, sizeof(value_msg), 
-                    "Adresse: 0x%08X, Written Value: %u, Received Value: %u\r\n", 
+                    "Warnung: Adresse: 0x%08X, Written Value: %u, Received Value: %u\r\n", 
                     addr, expected_value, read_value);
 
             uart_write_str(&__base_uart, value_msg, sizeof(value_msg));
             uart_write_flush(&__base_uart);
         }
+        iterations++;
     }
 
     uart_write_str(&__base_uart, finish_msg, sizeof(finish_msg));
