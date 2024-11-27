b402bf245dc8045a6a906d63510c2ef613f27e29
Raphael Roth <raroth@student.ethz.ch>
2024-11-27 22:40:33 +0100

    WIP disable optimization to avoid an error in packed typedefs

--- target/sim/vsim/start.cheshire_soc.tcl
+++ target/sim/vsim/start.cheshire_soc.tcl
@@ -24,7 +24,7 @@ if { ![info exists CXX_PATH] } {
 # Set voptargs only if not already set to make overridable.
 # Default on fast simulation flags.
 if { ![info exists VOPTARGS] } {
-    set VOPTARGS "-O5 +acc=p+tb_cheshire_soc. +noacc=p+cheshire_soc. +acc=r+stream_xbar"
+    set VOPTARGS "+acc"
 }
 
 set flags "-permissive -suppress 3009 -suppress 8386 -error 7 -cpppath ${CXX_PATH} "
