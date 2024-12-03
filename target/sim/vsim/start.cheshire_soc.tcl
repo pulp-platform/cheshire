e6fc609a23880e20486ddc60f73d222e17afdf50
sem24h18 Fabian Hauser (fhauser) <146116057+fhaus1@users.noreply.github.com>
2024-12-03 15:00:30 +0100

    Remove optimization

--- target/sim/vsim/start.cheshire_soc.tcl
+++ target/sim/vsim/start.cheshire_soc.tcl
@@ -24,8 +24,9 @@ if { ![info exists CXX_PATH] } {
 # Set voptargs only if not already set to make overridable.
 # Default on fast simulation flags.
 if { ![info exists VOPTARGS] } {
-    set VOPTARGS "-O5 +acc=p+tb_cheshire_soc. +noacc=p+cheshire_soc. +acc=r+stream_xbar"
+    set VOPTARGS "-O5 +acc=p+tb_cheshire_soc.  +acc=r+stream_xbar"
 }
+#+noacc=p+cheshire_soc.
 
 set flags "-permissive -suppress 3009 -suppress 8386 -error 7 -cpppath ${CXX_PATH} "
 if { [info exists SELCFG] } { append flags "-GSelectedCfg=${SELCFG} " }
