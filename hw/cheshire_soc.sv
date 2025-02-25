53a54a18ad70e3d6c5f959c9679d44ea2c070bcf
Riccardo Tedeschi <riccardo.tedeschi6@unibo.it>
2025-02-25 09:36:55 +0100

    hw/cheshire_soc.sv: use `XLEN` from `Cva6Cfg`

--- hw/cheshire_soc.sv
+++ hw/cheshire_soc.sv
@@ -611,8 +611,8 @@ module cheshire_soc import cheshire_pkg::*; #(
     ) i_core_cva6 (
       .clk_i,
       .rst_ni,
-      .boot_addr_i      ( BootAddr[cva6_config_pkg::CVA6ConfigXlen-1:0] ),
-      .hart_id_i        ( cva6_config_pkg::CVA6ConfigXlen'(i) ),
+      .boot_addr_i      ( BootAddr[Cva6Cfg.XLEN-1:0] ),
+      .hart_id_i        ( Cva6Cfg.XLEN'(i) ),
       .irq_i            ( xeip[i] ),
       .ipi_i            ( msip[i] ),
       .time_irq_i       ( mtip[i] ),
