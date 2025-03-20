5e60c7d5bf9121bb6e901594a81e3ebf22b1279b
Enrico Zelioli <ezelioli@iis.ee.ethz.ch>
2025-03-20 19:07:51 +0100

    Add vCLIC support
    - Bump CVA6 to ez/vclic branch
    - Bump CLIC to virt branch
    - Modify Cheshire package and soc to vCLIC

--- hw/cheshire_soc.sv
+++ hw/cheshire_soc.sv
@@ -598,6 +598,8 @@ module cheshire_soc import cheshire_pkg::*; #(
     logic [$clog2(NumClicIntrs)-1:0] clic_irq_id;
     logic [7:0]        clic_irq_level;
     riscv::priv_lvl_t  clic_irq_priv;
+    logic              clic_irq_v;
+    logic [5:0]        clic_irq_vsid;
 
     cva6 #(
       .CVA6Cfg        ( build_config_pkg::build_config(Cva6Cfg) ),
@@ -621,6 +623,8 @@ module cheshire_soc import cheshire_pkg::*; #(
       .clic_irq_id_i    ( clic_irq_id    ),
       .clic_irq_level_i ( clic_irq_level ),
       .clic_irq_priv_i  ( clic_irq_priv  ),
+      .clic_irq_v_i     ( clic_irq_v     ),
+      .clic_irq_vsid_i  ( clic_irq_vsid  ),
       .clic_irq_shv_i   ( clic_irq_shv   ),
       .clic_irq_ready_o ( clic_irq_ready ),
       .clic_kill_req_i  ( clic_irq_kill_req ),
@@ -675,12 +679,16 @@ module cheshire_soc import cheshire_pkg::*; #(
       };
 
       clic #(
-        .N_SOURCE   ( NumClicIntrs ),
-        .INTCTLBITS ( Cfg.ClicIntCtlBits ),
-        .reg_req_t  ( reg_req_t ),
-        .reg_rsp_t  ( reg_rsp_t ),
-        .SSCLIC     ( 1 ),
-        .USCLIC     ( 0 )
+        .N_SOURCE    ( NumClicIntrs ),
+        .INTCTLBITS  ( Cfg.ClicIntCtlBits ),
+        .reg_req_t   ( reg_req_t ),
+        .reg_rsp_t   ( reg_rsp_t ),
+        .SSCLIC      ( 1 ),
+        .USCLIC      ( 0 ),
+        .VSCLIC      ( Cfg.ClicVsclic ),
+        .N_VSCTXTS   ( Cfg.ClicNumVsctxts ),
+        .VSPRIO      ( Cfg.ClicVsprio ),
+        .VsprioWidth ( Cfg.ClicPrioWidth )
       ) i_clic (
         .clk_i,
         .rst_ni,
@@ -693,6 +701,8 @@ module cheshire_soc import cheshire_pkg::*; #(
         .irq_level_o    ( clic_irq_level ),
         .irq_shv_o      ( clic_irq_shv   ),
         .irq_priv_o     ( clic_irq_priv  ),
+        .irq_v_o        ( clic_irq_v     ),
+        .irq_vsid_o     ( clic_irq_vsid  ),
         .irq_kill_req_o ( clic_irq_kill_req ),
         .irq_kill_ack_i ( clic_irq_kill_ack )
       );
@@ -704,6 +714,8 @@ module cheshire_soc import cheshire_pkg::*; #(
       assign clic_irq_level    = '0;
       assign clic_irq_shv      = '0;
       assign clic_irq_priv     = riscv::priv_lvl_t'(0);
+      assign clic_irq_v        = '0;
+      assign clic_irq_vsid     = '0;
       assign clic_irq_kill_req = '0;
 
     end
