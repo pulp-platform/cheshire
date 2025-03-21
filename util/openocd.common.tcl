52253b021df4bb22cbf9eb6cc4c9e0754187f614
julia <git.ts@trainwit.ch>
2025-03-21 11:35:04 +1100

    save my openocd

--- util/openocd.common.tcl
+++ util/openocd.common.tcl
@@ -5,8 +5,8 @@
 # Common OpenOCD script for Cheshire.
 
 transport select jtag
-telnet_port disabled
-tcl_port disabled
+telnet port disabled
+tcl port disabled
 reset_config none
 
 set _CHIPNAME riscv
@@ -15,13 +15,12 @@ jtag newtap $_CHIPNAME cpu -irlen ${irlen} -expected-id 0x1c5e5db3
 set _TARGETNAME $_CHIPNAME.cpu
 target create $_TARGETNAME riscv -chain-position $_TARGETNAME -coreid 0
 
-gdb_report_data_abort enable
-gdb_report_register_access_error enable
+gdb report_data_abort enable
+gdb report_register_access_error enable
 
-riscv set_reset_timeout_sec 120
 riscv set_command_timeout_sec 120
 
-riscv set_prefer_sba off
+riscv set_mem_access progbuf abstract
 
 # Exit when debugger detaches
 $_TARGETNAME configure -event gdb-detach {
@@ -30,10 +29,14 @@ $_TARGETNAME configure -event gdb-detach {
 }
 
 # Try enabling address translation (only works for newer versions)
-if { [catch { riscv set_enable_virtual on } ] } {
-    echo "Warning: This version of OpenOCD does not support address translation.\
-        To debug on virtual addresses, please update to the latest version."
-}
+# if { [catch { riscv set_enable_virtual on } ] } {
+#     echo "Warning: This version of OpenOCD does not support address translation.\
+#         To debug on virtual addresses, please update to the latest version."
+# }
+
+# riscv set_enable_virt2phys off
+
+riscv virt2phys_mode sw
 
 init
 halt
