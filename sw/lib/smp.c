111b7f629e2b9b088a67444d26de020f9bfdb624
Paul Scheffler <paulsc@iis.ee.ethz.ch>
2025-04-13 22:55:25 +0200

    WIP

--- sw/lib/smp.c
+++ sw/lib/smp.c
@@ -4,45 +4,58 @@
 //
 // Emanuele Parisi <emanuele.parisi@unibo.it>
 // Enrico Zelioli <ezelioli@iis.ee.ethz.ch>
+// Paul Scheffler <paulsc@iis.ee.ethz.ch>
 
 #include "smp.h"
 
-void smp_resume(void) {
-    uint32_t num_harts = *reg32(&__base_regs, CHESHIRE_NUM_INT_HARTS_REG_OFFSET);
-    // Flush cache and wake-up all sleeping cores
-    fence();
-    for (uint32_t i = 1; i < num_harts; i++) {
-        *reg32(&__base_clint, i << 2) = 0x1;
-        while (*reg32(&__base_clint, i << 2))
-            ;
+#include "util.h"
+#include "regs/cheshire.h"
+#include "params.h"
+
+// Unpark specified harts and send them to a given destination.
+// Note that these harts are *not* set up and `dst` should set them up accordingly.
+// This is a runtime function and should not be used in general BMPs.
+void _smp_unpark(void* dst) {
+    // Only hart 0 should unpark harts
+    if (get_mhartid()) return;
+    // Set destination address.
+    *reg32(&__base_regs, CHESHIRE_SCRATCH_5_REG_OFFSET) = (uintptr_t)(dst) >> 32;
+    *reg32(&__base_regs, CHESHIRE_SCRATCH_4_REG_OFFSET) = (uintptr_t)(dst);
+    // Set SMP resume enable
+    *reg32(&__base_regs, CHESHIRE_SCRATCH_6_REG_OFFSET) = 1;
+    // Wake up harts
+    uint32_t num_int_harts = *reg32(&__base_regs, CHESHIRE_NUM_INT_HARTS_REG_OFFSET);
+    for (uint32_t i = 1; i < num_int_harts; i++) {
+        // Set interrupt
+        *reg32(&__base_clint, i << 2) = 1;
+        // Wait for interrupt pending to be cleared
+        while (*reg32(&__base_clint, i << 2)) {}
     }
+    // Unset SMP resume enable.
+    *reg32(&__base_regs, CHESHIRE_SCRATCH_6_REG_OFFSET) = 0;
 }
 
-// Shared variable for barrier synchronization
-static volatile uint64_t _barrier_target = 0;
-
-static void barrier_wait(volatile uint64_t *barrier, uint64_t incr, uint64_t reach) {
-    asm volatile("amoadd.d x6, %1, (%0)             \n"
-                 "2:                                \n"
-                 "fence                             \n"
-                 "ld     x6, 0(%0)                  \n"
-                 "bne    x6, %2, 2b                 \n"
-
-                 : /* output operands */
-                 : /* input operands */
-                 "r"(barrier), "r"(incr), "r"(reach)
-                 : /* clobbered registers */
-                 "x6");
+// Park all active nonzero harts entering this function by sending them to the boot ROM.
+// This is a runtime function and should not be used in general BMPs.
+void _smp_park() {
+    if (get_mhartid() == 0) return;
+    invoke((void *)(uintptr_t)&__base_bootrom);
 }
 
-void smp_barrier_init() {
-    _barrier_target = 0;
+smp_sema_t smp_sema_get(int sid) {
+    // We can only allocate as many semaphores as we have platform scratch registers.
+    if (sid < 0 || sid >= CHESHIRE_SCRATCH_MULTIREG_COUNT - 8) return NULL;
+    return reg32(&__base_regs, CHESHIRE_SCRATCH_8_REG_OFFSET)[sid];
 }
 
-void smp_barrier_up(uint64_t n_processes) {
-    barrier_wait(&_barrier_target, 1, n_processes);
+void smp_sema_wait(smp_sema_t sema, int value, uint64_t spin_period) {
+    while (*sema != value) { for (uint64_t i = 0; i < spin_period; ++i) nop(); }
 }
 
-void smp_barrier_down() {
-    barrier_wait(&_barrier_target, -1, 0);
+void smp_barrier(uint64_t spin_period) {
+    uint32_t num_int_harts = *reg32(&__base_regs, CHESHIRE_NUM_INT_HARTS_REG_OFFSET);
+    volatile uint32_t *sema = reg32(&__base_regs, CHESHIRE_SCRATCH_7_REG_OFFSET);
+    __atomic_fetch_add(sema, 1, __ATOMIC_RELAXED);
+    smp_sema_wait(-1, num_int_harts, spin_period);
+    *sema = 0;
 }
--- sw/tests/smp_hello.c
+++ sw/tests/smp_hello.c
@@ -6,53 +6,37 @@
 // Christopher Reinwardt <creinwar@student.ethz.ch>
 // Emanuele Parisi <emanuele.parisi@unibo.it>
 // Enrico Zelioli <ezelioli@iis.ee.ethz.ch>
+// Paul Scheffler <paulsc@iis.ee.ethz.ch>
 //
 // Simple SMP Hello World.
 
-#include "regs/cheshire.h"
-#include "dif/clint.h"
-#include "dif/uart.h"
-#include "params.h"
-#include "util.h"
-#include "smp.h"
-#include "printf.h"
+int smain(int hid, int hnum) {
+    // Define a reasonable semaphore spin period to reduce contention.
+    const uint64_t SP = 20;
 
-uint32_t __attribute__((section(".data"))) semaphore = 0x0;
+    // Get and check SMP semaphore.
+    smp_sema_t sema = smp_sema_get(0);
+    CHECK_ASSERT(-1, sema != NULL)
 
-void semaphore_wait() {
-    asm volatile("   li t0, 1                    \n"
-                 "1:                             \n"
-                 "   amoswap.w.aq t0, t0, (%0)   \n"
-                 "   bnez t0, 1b                 \n" ::"r"(&semaphore));
-}
-
-void semaphore_post() {
-    asm volatile("   amoswap.w.rl zero, zero, (%0)   \n" ::"r"(&semaphore));
-}
-
-int main(void) {
-
-    uint64_t hart_id = get_mhartid();
-    uint32_t num_harts = *reg32(&__base_regs, CHESHIRE_NUM_INT_HARTS_REG_OFFSET);
-
-    if (hart_id == 0) {
+    // Only hart 0 initializes UART and semaphore.
+    // A barrier ensures that all harts wait for initialization.
+    if (hid == 0) {
         uint32_t rtc_freq = *reg32(&__base_regs, CHESHIRE_RTC_FREQ_REG_OFFSET);
         uint64_t reset_freq = clint_get_core_freq(rtc_freq, 2500);
         uart_init(&__base_uart, reset_freq, __BOOT_BAUDRATE);
-        smp_barrier_init();
-        smp_resume();
+        *sema = 0;
     }
+    smp_barrier(SP);
 
-    smp_barrier_up(num_harts);
+    // Let each hart print sequentially using a atomically incremented semaphore.
+    // Finally, use the semaphore to wait until the last hart has printed.
+    smp_sema_wait(sema, hid, SP);
+    printf("Hi from hart %d/%d\r\n", hid, hnum);
+    uart_write_flush(&__base_uart);
+    __atomic_fetch_add(sema, 1, __ATOMIC_RELAXED);
+    smp_sema_wait(sema, hnum, SP);
 
-    for (uint64_t i = 0; i < 1; i++) {
-        semaphore_wait();
-        printf("Core %d/%d up\n", hart_id, num_harts);
-        uart_write_flush(&__base_uart);
-        semaphore_post();
-    }
-
-    smp_barrier_down();
-
-    return 0;
+    // Double-check that our semaphore is now equal to the internal hart count.
+    // Only hart 0's return code is checked.
+    return (*sema == hnum);
 }
