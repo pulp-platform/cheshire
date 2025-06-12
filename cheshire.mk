03a29a94f34681aa1f16c2507d0ef97007ccd875
Thomas Benz <tbenz@iis.ee.ethz.ch>
2025-06-12 16:58:23 +0200

    Activate HPDcache; enable FlushonFence and InvalidateOnFlush

--- cheshire.mk
+++ cheshire.mk
@@ -16,7 +16,7 @@ VLOG_ARGS   ?= -suppress 2583 -suppress 13314 -timescale 1ns/1ps
 VLOGAN_ARGS ?= -kdb -nc -assert svaext +v2k -timescale=1ns/1ps
 
 # Common Bender flags for Cheshire RTL
-CHS_BENDER_RTL_FLAGS ?= -t rtl -t cva6 -t cv64a6_imafdchsclic_sv39_wb
+CHS_BENDER_RTL_FLAGS ?= -t rtl -t cva6 -t cv64a6_imafdchsclic_sv39_hpdcache
 
 # Define used paths (prefixed to avoid name conflicts)
 CHS_ROOT      ?= $(shell $(BENDER) path cheshire)
--- hw/cheshire_pkg.sv
+++ hw/cheshire_pkg.sv
@@ -516,6 +516,9 @@ package cheshire_pkg;
     ret.BTBEntries            = cfg.Cva6BTBEntries;
     ret.BHTEntries            = cfg.Cva6BHTEntries;
     ret.NrPMPEntries          = cfg.Cva6NrPMPEntries;
+    // Hacky shit
+    ret.DcacheFlushOnFence      = 1;
+    ret.DcacheInvalidateOnFlush = 1;
     // Return modified config
     return ret;
   endfunction
