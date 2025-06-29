d4c6a48cb4c2e2ca5ae51d7e00e00660017d08a1
Yvan Tortorella <ytortorella@lagrev5.ee.ethz.ch>
2025-06-29 21:51:35 +0200

    Bump nonfree.

--- cheshire.mk
+++ cheshire.mk
@@ -63,7 +63,7 @@ chs-clean-deps:
 ######################
 
 CHS_NONFREE_REMOTE ?= git@iis-git.ee.ethz.ch:pulp-restricted/cheshire-nonfree.git
-CHS_NONFREE_COMMIT ?= ad9f538 # yt/coremark
+CHS_NONFREE_COMMIT ?= 761c7f1 # yt/coremark
 
 CHS_PHONY += chs-nonfree-init
 chs-nonfree-init:
