8a76e5c867dfc3565f4eef26d8584045fc3d074a
aottaviano <aottaviano@iis.ee.ethz.ch>
2025-07-16 13:43:11 +0200

    cheshire.mk: Bump nonfree
    Update I2C error filter to filter for different timescales, as it can
    currently filter only `ns`

--- cheshire.mk
+++ cheshire.mk
@@ -63,7 +63,7 @@ chs-clean-deps:
 ######################
 
 CHS_NONFREE_REMOTE ?= git@iis-git.ee.ethz.ch:pulp-restricted/cheshire-nonfree.git
-CHS_NONFREE_COMMIT ?= 1e80dbc
+CHS_NONFREE_COMMIT ?= 50b43f1
 
 CHS_PHONY += chs-nonfree-init
 chs-nonfree-init:
