cc6dd9f8732a71310f8eed2b328ad34b8d92db03
aottaviano <aottaviano@iis.ee.ethz.ch>
2025-07-17 14:25:34 +0200

    cheshire.mk: Bump nonfree

--- cheshire.mk
+++ cheshire.mk
@@ -63,7 +63,7 @@ chs-clean-deps:
 ######################
 
 CHS_NONFREE_REMOTE ?= git@iis-git.ee.ethz.ch:pulp-restricted/cheshire-nonfree.git
-CHS_NONFREE_COMMIT ?= 50b43f1
+CHS_NONFREE_COMMIT ?= e03e0ab
 
 CHS_PHONY += chs-nonfree-init
 chs-nonfree-init:
