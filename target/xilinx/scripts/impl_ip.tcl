1f66f7c647e373e84b89738c9ab93db066ddb0eb
Yvan Tortorella <ytortorella@lagrev5.ee.ethz.ch>
2025-08-06 21:45:53 +0200

    Add VCU118 to DOC.

--- target/xilinx/scripts/impl_ip.tcl
+++ target/xilinx/scripts/impl_ip.tcl
@@ -142,18 +142,7 @@ switch $proj {
                     CONFIG.C_NUM_PROBE_IN {0} \
                     ] [get_ips $proj]
             }
-            vcu128 {
-                set_property -dict [list \
-                    CONFIG.C_NUM_PROBE_OUT {3} \
-                    CONFIG.C_PROBE_OUT0_INIT_VAL {0x0} \
-                    CONFIG.C_PROBE_OUT1_INIT_VAL {0x2} \
-                    CONFIG.C_PROBE_OUT2_INIT_VAL {0x1} \
-                    CONFIG.C_PROBE_OUT1_WIDTH {2} \
-                    CONFIG.C_EN_PROBE_IN_ACTIVITY {0} \
-                    CONFIG.C_NUM_PROBE_IN {0} \
-                    ] [get_ips $proj]
-            }
-            vcu118 {
+            {vcu118 vcu128} {
                 set_property -dict [list \
                     CONFIG.C_NUM_PROBE_OUT {3} \
                     CONFIG.C_PROBE_OUT0_INIT_VAL {0x0} \
