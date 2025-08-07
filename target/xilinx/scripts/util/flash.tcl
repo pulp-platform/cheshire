cfbd59a31c150ba99f56482a621ce6adf6ea38fd
Yvan Tortorella <ytortorella@lagrev5.ee.ethz.ch>
2025-08-07 08:28:56 +0200

    Properly use TCL switches.

--- target/xilinx/scripts/util/flash.tcl
+++ target/xilinx/scripts/util/flash.tcl
@@ -22,7 +22,8 @@ set hw_cfgmem [get_property PROGRAM.HW_CFGMEM $hw_device]
 # Create image for and configure memory depending on board
 switch $board {
     genesys2 -
-    {vcu118 vcu128} {
+    vcu118   -
+    vcu128   {
         set mcs ${project_root}/image.mcs
         write_cfgmem -force -format mcs -size 256 -interface SPIx4 \
             -loaddata "up $offs $file" -checksum -file $mcs
