ffff456e27f39a18e11993e45a0a6888a9146753
krabch <abdelkadir.chantar@tuni.fi>
2025-09-04 17:11:40 +0300

    lint-license: check license

--- util/openocd.zcu104.tcl
+++ util/openocd.zcu104.tcl
@@ -3,10 +3,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 # OpenOCD script for Cheshire on ZCU104.
-
 adapter driver ftdi
 adapter serial FT5X1XMQ;
-
 adapter speed 1000
 ftdi vid_pid 0x0403 0x6010
 ftdi layout_init 0x0088 0x008b
