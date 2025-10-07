e4d980ba105db7fc489cb91b3e48c4793a7e2cca
Paul Scheffler <paulsc@iis.ee.ethz.ch>
2025-10-07 20:59:05 +0200

    sw: Add some tests for verilator exploration

---
+++ sw/tests/uart_fast.c
@@ -0,0 +1,26 @@
+// Copyright 2023 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Nicole Narr <narrn@student.ethz.ch>
+// Christopher Reinwardt <creinwar@student.ethz.ch>
+// Paul Scheffler <paulsc@iis.ee.ethz.ch>
+
+#include "regs/cheshire.h"
+#include "dif/clint.h"
+#include "dif/uart.h"
+#include "params.h"
+#include "util.h"
+
+int main(void) {
+    char str[] =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor\r\n"
+        "incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis\r\n"
+        "nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\r\n";
+    uint32_t rtc_freq = *reg32(&__base_regs, CHESHIRE_RTC_FREQ_REG_OFFSET);
+    uint64_t reset_freq = clint_get_core_freq(rtc_freq, 2500);
+    uart_init(&__base_uart, reset_freq, __BOOT_BAUDRATE * 8);
+    uart_write_str(&__base_uart, str, sizeof(str) - 1);
+    uart_write_flush(&__base_uart);
+    return 0;
+}
