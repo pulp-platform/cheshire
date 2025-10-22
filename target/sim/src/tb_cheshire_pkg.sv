0a369e0c72a2a2b2bcf03739cefd14f019ab8440
Enrico Zelioli <ezelioli@iis.ee.ethz.ch>
2025-10-22 15:17:08 +0200

    tb: extend RT configuration
    - Add vCLIC to RT configuration
    - Add TLB coloring/locking to RT configuration

--- target/sim/src/tb_cheshire_pkg.sv
+++ target/sim/src/tb_cheshire_pkg.sv
@@ -12,7 +12,18 @@ package tb_cheshire_pkg;
     // A dedicated RT config
     function automatic cheshire_cfg_t gen_cheshire_rt_cfg();
       cheshire_cfg_t ret = DefaultCfg;
+      ret.Cva6InstrTlbEntries = 16;
+      ret.Cva6DataTlbEntries = 64;
+      ret.Cva6TlbColoring = 1;
+      ret.Cva6NumTlbColors = 16;
+      ret.Cva6LockableTlbWays = 8;
+      ret.Cva6UseSharedTlb = 0;
       ret.AxiRt = 1;
+      ret.Clic = 1;
+      ret.ClicVsclic = 1;
+      ret.ClicVsprio = 1;
+      ret.ClicNumVsctxts = 4;
+      ret.ClicPrioWidth = 1;
       return ret;
     endfunction
 
