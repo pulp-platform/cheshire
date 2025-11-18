bd9956b37fa2497e31a7ea191bc285b953b484d5
yvantor <yvan.tortorella@gmail.com>
2025-11-18 16:49:07 +0100

    Alow all parameters to be passed by command line.

--- target/sim/xcelium/start.cheshire.soc.sh
+++ target/sim/xcelium/start.cheshire.soc.sh
@@ -3,7 +3,7 @@
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 #
-# Cyril Koenig <cykoenig@iis.ee.ethz.ch>
+# Yvan Tortorella <yvan.tortorella@gmail.com>
 
 TESTBENCH=tb_cheshire_soc
 
@@ -19,39 +19,39 @@ fi
 [[ -z "${XCELIUM_VERSION}" ]] && XCELIUM_VERSION=""
 [[ -z "${XCELIUM_BIN}" ]]     && XCELIUM_BIN="${XCELIUM_VERSION} xrun"
 
-# # Set default to fast simulation flags.
-# if [ -z "${XCELIUMARGS}" ]; then
-#     # Use -debug_access+all for waveform debugging
-#     flags+="-O2 -debug_access=r -debug_region=1,${TESTBENCH} "
-# fi
+# Set default to fast simulation flags.
+flags="-timescale 1ns/1ps -64bit -sv "
+if [ -z "${XCELIUMARGS}" ]; then
+    # Use -access +rwc for waveform debugging
+    flags+="-access +r -enable_typeparam_cfn "
+fi
 
-# flags+="-cpp ${CXX_PATH} "
-# [[ -n "${SELCFG}" ]]   && flags+="-pvalue+SelectedCfg=${SELCFG} "
+if [ -n "${SELCFG}" ]; then
+    flags+="-gpg "SelectedCfg => ${SELCFG}" "
+    pargs="-gpg "SelectedCfg => ${SELCFG}" "
+fi
 
-# pargs=""
-[[ -n "${BOOTMODE}" ]] && pargs+="+BOOTMODE=${BOOTMODE} "
-[[ -n "${PRELMODE}" ]] && pargs+="+PRELMODE=${PRELMODE} "
-[[ -n "${BINARY}" ]]   && pargs+="+BINARY=${BINARY} "
-[[ -n "${IMAGE}" ]]    && pargs+="+IMAGE=${IMAGE} "
+[[-n "${USE_GUI}"]]      && pargs+="-gui "
+[[ -n "${BOOTMODE}" ]]   && pargs+="+BOOTMODE=${BOOTMODE} "
+[[ -n "${PRELMODE}" ]]   && pargs+="+PRELMODE=${PRELMODE} "
+[[ -n "${BINARY}" ]]     && pargs+="+BINARY=${BINARY} "
+[[ -n "${IMAGE}" ]]      && pargs+="+IMAGE=${IMAGE} "
 
 # # DRAMSys
-# if [ -n "${USE_DRAMSYS}" ]; then
-#     flags+="-pvalue UseDramSys=${USE_DRAMSYS} "
-#     if [[ "${USE_DRAMSYS}" == 1 ]]; then
-#         DRAMSYS_ROOT="../dramsys"
-#         DRAMSYS_LIB="${DRAMSYS_ROOT}/build/lib"
-#         pargs+="+DRAMSYS_RES=${DRAMSYS_ROOT}/configs "
-#         pargs+="-sv_lib ${DRAMSYS_LIB}/libDRAMSys_Simulator "
-#     fi
-# fi
-
-TS="-timescale 1ns/1ps"
-COMPILE_ARGS="-64bit -sv -licqueue -access +rwc -messages -smartorder -v200x -enable_inout_out_def_arg -enable_typeparam_cfn"
+if [ -n "${USE_DRAMSYS}" ]; then
+    flags+="-gpg UseDramSys=${USE_DRAMSYS} "
+    if [[ "${USE_DRAMSYS}" == 1 ]]; then
+        DRAMSYS_ROOT="../dramsys"
+        DRAMSYS_LIB="${DRAMSYS_ROOT}/build/lib"
+        pargs+="+DRAMSYS_RES=${DRAMSYS_ROOT}/configs "
+        pargs+="-sv_lib ${DRAMSYS_LIB}/libDRAMSys_Simulator "
+    fi
+fi
 
 # Obtain path to XCelium includes to compile DPIs
 XCELIUM_INC="$(dirname "$(dirname "$(realpath "$(which ${XCELIUM_BIN})")")")/include"
 mkdir dpilib && ${CXX} ../src/elfloader.cpp -fpic -shared -o dpilib/libmydpi.so -I${XCELIUM_INC}
 
-${XCELIUM_BIN} -elaborate ${TS} ${COMPILE_ARGS} -f compile.cheshire_soc.f -top ${TESTBENCH} -create_sharedlib -lmydpi -L./dpilib -snapshot ${TESTBENCH}_snap
-${XCELIUM_BIN} -gui ${TS} ${pargs} -R -snapshot ${TESTBENCH}_snap ${PLUSARGS}
+${XCELIUM_BIN} -elaborate ${flags} -f compile.cheshire_soc.f -top ${TESTBENCH} -create_sharedlib -lmydpi -L./dpilib -snapshot ${TESTBENCH}_snap
+${XCELIUM_BIN} ${pargs} -R -snapshot ${TESTBENCH}_snap
 
