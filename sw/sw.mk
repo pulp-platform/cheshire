6020ebc0ac7bc170f27847c1ae18aa81bcd817c2
Tim Fischer <fischeti@iis.ee.ethz.ch>
2026-03-04 21:28:48 +0100

    hw(regs): Convert cheshire_regs from hjson to SystemRDL
    Replace the lowRISC regtool-based hjson register description with a
    SystemRDL source and peakrdl-regblock generated RTL. The generated
    register block uses an APB4-flat CPU interface; a reg_to_apb bridge is
    added in cheshire_soc to convert from the internal reg_iface demux.
    
    - Add hw/regs/rdl/cheshire_regs.rdl (register source)
    - Generate hw/regs/rtl/cheshire_regs{,_pkg}.sv via peakrdl regblock
    - Update cheshire.mk: PEAKRDL_INCLUDES, replace regtool make rule
    - Update sw/sw.mk: switch cheshire.h to chs_sw_gen_hdr_rule_rdl
    - Update Bender.yml: point to new rtl/ file locations
    - Update hw/cheshire_soc.sv: add reg_to_apb bridge, new pkg types
    - Delete hw/regs/cheshire_regs.hjson and hjson-generated SV files

--- sw/sw.mk
+++ sw/sw.mk
@@ -91,7 +91,7 @@ $(eval $(call chs_sw_gen_hdr_rule,serial_link,$(CHS_ROOT)/hw/serial_link.hjson $
 $(eval $(call chs_sw_gen_hdr_rule,axi_vga,$(AXI_VGA_ROOT)/data/axi_vga.hjson $(AXI_VGA_ROOT)/.generated))
 $(eval $(call chs_sw_gen_hdr_rule,idma,$(IDMA_ROOT)/target/rtl/idma_reg64_2d.hjson))
 $(eval $(call chs_sw_gen_hdr_rule,axi_llc,$(CHS_LLC_DIR)/data/axi_llc_regs.hjson))
-$(eval $(call chs_sw_gen_hdr_rule,cheshire,$(CHS_ROOT)/hw/regs/cheshire_regs.hjson))
+$(eval $(call chs_sw_gen_hdr_rule_rdl,cheshire,$(CHS_ROOT)/hw/regs/cheshire_regs.rdl))
 $(eval $(call chs_sw_gen_hdr_rule,axi_rt,$(AXIRTROOT)/src/regs/axi_rt.hjson $(AXIRTROOT)/.generated))
 
 # Generate headers for OT peripherals in the bendered repo itself
