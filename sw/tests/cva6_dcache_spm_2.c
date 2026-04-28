e61c8a568e08a2e61d7f2cdaf58a6ede2fdac46f
Riccardo Tedeschi <riccardo.tedeschi6@unibo.it>
2026-04-28 18:04:23 +0200

    sw: fix DSPM test

--- sw/tests/cva6_dcache_spm_2.c
+++ sw/tests/cva6_dcache_spm_2.c
@@ -19,19 +19,19 @@
 #define N 1024
 #define data_t uint64_t
 
-volatile data_t *spm_data_1 = (volatile data_t *)(&__l1_dspm_base_addr__ + 0 * N * sizeof(data_t));
-volatile data_t *spm_data_2 = (volatile data_t *)(&__l1_dspm_base_addr__ + 1 * N * sizeof(data_t));
+volatile data_t *spm_data_1 = (volatile data_t *)&__l1_dspm_base_addr__ + 0 * N;
+volatile data_t *spm_data_2 = (volatile data_t *)&__l1_dspm_base_addr__ + 1 * N;
 
 data_t data_1[N];
 data_t data_2[N];
 
-void axpy(data_t a, data_t *x, data_t *y, data_t n) {
+void axpy(data_t a, volatile data_t *x, volatile data_t *y, data_t n) {
     for (data_t i = 0; i < n; i++) {
         y[i] += a * x[i];
     }
 }
 
-void init_data(data_t *data, data_t n, data_t seed) {
+void init_data(volatile data_t *data, data_t n, data_t seed) {
     data[0] = seed;
     for (data_t i = 1; i < n; i++) {
         data[i] = data[i - 1] * i;
@@ -46,7 +46,7 @@ void enable_spm(uint32_t num_ways) {
     // Disable D-cache
     asm volatile("csrwi 0x7C1, 0");
     // Flush the D-cache
-    asm volatile("fence");
+    asm volatile("fence.i");
     // Configure four ways as scratchpad
     uint32_t way_mask = (1 << num_ways) - 1;
     asm volatile("csrw 0x5E0, %0" : : "r"(way_mask));
@@ -55,8 +55,6 @@ void enable_spm(uint32_t num_ways) {
 }
 
 int main(void) {
-
-    data_t result_cache, result_spm;
 
     init_data(data_1, N, 1);
     init_data(data_2, N, 2);
