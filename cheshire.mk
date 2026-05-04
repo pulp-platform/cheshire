322ed527ade0d72d97bd7d6e63fc4c966e6655e8
Cyril Koenig <cyril27koenig@gmail.com>
2026-05-04 11:43:28 +0200

    feat(CI): Build bitstream for default and hpdcache configs

--- cheshire.mk
+++ cheshire.mk
@@ -65,7 +65,7 @@ chs-clean-deps:
 ######################
 
 CHS_NONFREE_REMOTE ?= git@iis-git.ee.ethz.ch:pulp-restricted/cheshire-nonfree.git
-CHS_NONFREE_COMMIT ?= 68f8891d1c597e1f3efb219530b7bb06a9a767de
+CHS_NONFREE_COMMIT ?= c847443
 
 CHS_PHONY += chs-nonfree-init
 chs-nonfree-init:
