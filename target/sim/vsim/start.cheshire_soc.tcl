1104fac362aee8ad3b880fc84d44ab5480dbb5cf
Yizhen Xu <linqixu@ethz.ch>
2026-08-25 18:21:37 +0200

    add openocd option

--- target/sim/vsim/start.cheshire_soc.tcl
+++ target/sim/vsim/start.cheshire_soc.tcl
@@ -12,43 +12,27 @@ set TESTBENCH tb_cheshire_soc
 # Set voptargs only if not already set to make overridable.
 # Default on fast simulation flags.
 if {![info exists VOPTARGS]} {
-    #set VOPTARGS "-O5 +acc=p+tb_cheshire_soc. +noacc=p+cheshire_soc. +acc=r+stream_xbar"
-    set VOPTARGS "+acc"
-    #set VOPTARGS "-O5"
+    set VOPTARGS "-O5 +acc=p+tb_cheshire_soc. +noacc=p+cheshire_soc. +acc=r+stream_xbar"
 }
 
 set flags "-permissive -suppress 3009 -suppress 8386 -error 7 "
-
-if {[info exists SELCFG]} {
-    append flags "-GSelectedCfg=${SELCFG} "
-}
+if {[info exists SELCFG]} { append flags "-GSelectedCfg=${SELCFG} " }
 
 set pargs ""
-
-if {[info exists BOOTMODE]} {
-    append pargs "+BOOTMODE=${BOOTMODE} "
-}
-if {[info exists PRELMODE]} {
-    append pargs "+PRELMODE=${PRELMODE} "
-}
-if {[info exists BINARY]} {
-    append pargs "+BINARY=${BINARY} "
-}
-if {[info exists IMAGE]} {
-    append pargs "+IMAGE=${IMAGE} "
-}
+if {[info exists BOOTMODE]} { append pargs "+BOOTMODE=${BOOTMODE} " }
+if {[info exists PRELMODE]} { append pargs "+PRELMODE=${PRELMODE} " }
+if {[info exists BINARY]}   { append pargs "+BINARY=${BINARY} " }
+if {[info exists IMAGE]}    { append pargs "+IMAGE=${IMAGE} " }
 
 if {[info exists OPENOCD] && $OPENOCD} {
     # Questa expects the shared-library path without the .so suffix.
     set script_dir [file dirname [file normalize [info script]]]
     set simjtag_dpi [file join ${script_dir} simjtag_dpi]
-
     # append flags "-sv_lib ${simjtag_dpi} "
     append pargs "+OPENOCD "
 }
 
-eval "vsim -c ${TESTBENCH} -t 1ps -vopt -voptargs=\"${VOPTARGS}\"" \
-    ${pargs} ${flags}
+eval "vsim -c ${TESTBENCH} -t 1ps -vopt -voptargs=\"${VOPTARGS}\"" ${pargs} ${flags}
 
 set StdArithNoWarnings 1
 set NumericStdNoWarnings 1
